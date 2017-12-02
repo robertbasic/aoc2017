6077566eddc475965ee3bb85ee34a566576e68a4
Robert Basic <robertbasic.com@gmail.com>
2017-12-02 06:40:11 +0100

    Bring some order to the chaos

---
+++ day1_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+var sumtests = []struct {
+	in  string
+	out int
+}{
+	{"1122", 3},
+	{"1111", 4},
+	{"1212", 0},
+	{"1234", 0},
+	{"2222", 8},
+	{"91212129", 9},
+}
+
+var halftests = []struct {
+	in  string
+	out int
+}{
+	{"1111", 4},
+	{"1212", 6},
+	{"1221", 0},
+	{"2222", 8},
+	{"123425", 4},
+	{"123123", 12},
+}
+
+func TestSum(t *testing.T) {
+	for _, tt := range sumtests {
+		s := Sum(tt.in)
+
+		if s != int64(tt.out) {
+			t.Errorf("Got %d for %s, expected %d", s, tt.in, tt.out)
+		}
+	}
+}
+
+func TestHalf(t *testing.T) {
+	for _, tt := range halftests {
+		h := Half(tt.in)
+
+		if h != int64(tt.out) {
+			t.Errorf("Got %d for %s, expected %d", h, tt.in, tt.out)
+		}
+	}
+}
