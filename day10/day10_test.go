d39da39f7fecf82238120f5fc5d868372b08eeb3
Robert Basic <robertbasic.com@gmail.com>
2017-12-15 12:32:40 +0100

    Add test for ElvenKnottedHash

--- day10/day10_test.go
+++ day10/day10_test.go
@@ -6,11 +6,22 @@ import (
 	"testing"
 )
 
+func TestElvenKnottedHash(t *testing.T) {
+	input := "34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167"
+	r := ElvenKnottedHash(input)
+
+	e := "a7af2706aa9a09cf5d848c1e6605dd2a"
+
+	if r != e {
+		t.Errorf("Got %s, expected %s", r, e)
+	}
+}
+
 func TestElvenHashChecksum(t *testing.T) {
 	var listSize = 5
 	var lengths = []int{3, 4, 1, 5}
 
-	r := ElvenHashChecksum(listSize, lengths, 1)
+	r := elvenHashChecksum(listSize, lengths, 1)
 
 	e := 12
 
